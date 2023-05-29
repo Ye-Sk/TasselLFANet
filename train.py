5e7d7aca9979c4d4e2eb55f4ac9f073d5b8eece4
Jianxiong Ye <110390404+Ye-Sk@users.noreply.github.com>
2023-05-29 08:56:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -10,12 +10,11 @@ def config():
     parser.add_argument('--batch-size', type=int, default=16, help='size of each image batch')
     parser.add_argument('--workers', type=int, default=8, help='number of dataloader workers')
     parser.add_argument('--dataset', type=str, default='config/data.yaml', help='config.yaml path')
-    parser.add_argument('--cache', action='store_true', help='cache images for faster training')
+    parser.add_argument('--cache', default=False, help='cache images for faster training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
     parser.add_argument('--save-dir', default='runs/train', help='save results path(s)')
     cfg = parser.parse_args()
     return cfg
 
-if __name__ == '__main__':
-    cfg = config()
-    train(cfg)
+cfg = config()
+train(cfg)
